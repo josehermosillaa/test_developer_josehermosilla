80e125a41f0db3d28775344217eae885bd124570
josehermosillaa <jose.hermosillaa@usach.cl>
2023-02-16 11:27:27 -0300

    se cambian el motor de base de datos a psql en settings.py

--- test_developer/test_developer/settings.py
+++ test_developer/test_developer/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = "test_developer.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
